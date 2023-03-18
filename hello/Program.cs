95432ba224a087beed4fd774fdb93c3b0999418d
Andrew Quinn <53230903+hiAndrewQuinn@users.noreply.github.com>
2023-03-18 19:13:27 +0200

    Initial commit

---
+++ hello/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("I know absolutely nothing about C#...");
+Console.WriteLine("... but I'm excited to start learning!");
+
+Console.WriteLine("I'm going to be a C# developer!");
